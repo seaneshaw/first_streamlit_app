50369b89f721c996e0e512d66ddafb108f7f80fb
seaneshaw <107632213+seaneshaw@users.noreply.github.com>
2022-06-16 10:50:09 -0400

    Update streamlit_app.py
    Miss closing quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My parents new healthy diner)
+streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
